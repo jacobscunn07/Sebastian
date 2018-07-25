8f1d14309d4391d5702b66ac274cd8520c70f3f5
Jacob Cunningham <jacob.cunningham@live.com>
2018-07-24 22:45:38 -0500

    Added rh.exe to tools folder and created ps tasks for updating database

--- Sebastian.Build.ps1
+++ Sebastian.Build.ps1
@@ -1,7 +1,14 @@
 $basePath = Get-Location
 $srcPath = "$basePath\src"
-$configuration = 'Release'
+$targetFramework = "netcoreapp2.1"
+$configuration = 'Debug'
 $testsProjPath = "$srcPath\Sebastian.Tests"
+$databaseProjPath = "$srcPath\Sebastian.Database"
+
+$connectionStrings = @{
+    DEV = "Server=.;Database=Sebastian_Dev;Trusted_Connection=True;";
+    TEST = "Server=.;Database=Sebastian_Test;Trusted_Connection=True;";
+}
 
 Task Build -Before Run-Tests {
     Set-Location -Path $srcPath
@@ -13,4 +20,36 @@ Task Run-Tests {
     Set-Location -Path $testsProjPath
     dotnet fixie
     Set-Location -Path $basePath
+}
+
+Task Update-Dev-Database {
+    Update-Database DEV
+}
+
+Task Update-Test-Database {
+    Update-Database TEST
+}
+
+function Update-Database([Parameter(ValueFromRemainingArguments)]$environments) {
+    $migrationsProject =  "Sebastian.Database"
+    $roundhouseExePath = "$basepath\tools\rh.exe"
+    $roundhouseOutputDir = [System.IO.Path]::GetDirectoryName($roundhouseExePath) + "\output"
+
+    $migrationScriptsPath ="/"
+    $roundhouseVersionFile = "$srcPath\Sebastian.Database\bin\$configuration\$targetFramework\$migrationsProject.dll"
+
+    foreach ($environment in $environments) {
+        $connectionString = $connectionStrings[$environment]
+
+    Write-Host "Executing RoundhousE for environment:" $environment
+
+    exec { & $roundhouseExePath --connectionstring $connectionString `
+                                    --commandtimeout 300 `
+                                    --env $environment `
+                                    --output $roundhouseOutputDir `
+                                    --sqlfilesdirectory $migrationScriptsPath `
+                                    --versionfile $roundhouseVersionFile `
+                                    --transaction `
+                                    --silent }
+    }
 }
