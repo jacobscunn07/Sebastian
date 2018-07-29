aa1ba266ebbc9994bb3cfe871cd7a84f2b797fdf
Jacob Cunningham <jacob.cunningham@live.com>
2018-07-28 22:51:55 -0500

    Created database schema for entire workout tree

---
+++ src/Sebastian.Database/up/0001_CreateExerciseTables.sql
@@ -0,0 +1,38 @@
+-- Reps
+-- Pounds
+CREATE TABLE ExerciseTypeAttribute (
+  Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_ExerciseTypeAttribute_Id PRIMARY KEY
+		CONSTRAINT DF_ExerciseTypeAttribute_Id DEFAULT NEWID()
+  , [Name] VARCHAR(128) NOT NULL
+)
+
+-- Weighted Reps
+-- Assisted Reps
+-- Distance
+CREATE TABLE ExerciseType (
+  Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_ExerciseType_Id PRIMARY KEY
+		CONSTRAINT DF_ExerciseType_Id DEFAULT NEWID()
+  , [Name] VARCHAR(128) NOT NULL
+)
+
+CREATE TABLE ExerciseType_ExerciseTypeAttribute (
+  Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_ExerciseTypeExerciseTypeAttribute_Id PRIMARY KEY
+		CONSTRAINT DF_ExerciseTypeExerciseTypeAttributes_Id DEFAULT NEWID()
+  , ExerciseTypeId UNIQUEIDENTIFIER NOT NULL 
+  , ExerciseTypeAttributeId UNIQUEIDENTIFIER NOT NULL 
+  , CONSTRAINT FK_ExerciseTypeExerciseTypeAttribute_ExerciseTypeId FOREIGN KEY (ExerciseTypeId) REFERENCES ExerciseType(Id)
+  , CONSTRAINT FK_ExerciseTypeExerciseTypeAttribute_ExerciseTypeAttributeId FOREIGN KEY (ExerciseTypeAttributeId) REFERENCES ExerciseTypeAttribute(Id)
+)
+
+-- Bench Press
+CREATE TABLE Exercise (
+  Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_Exercise_Id PRIMARY KEY
+		CONSTRAINT DF_Exercise_Id DEFAULT NEWID()
+  , ExerciseTypeId UNIQUEIDENTIFIER NOT NULL 
+  , [Name] VARCHAR(128) NOT NULL
+  , CONSTRAINT FK_Exercise_ExerciseTypeId FOREIGN KEY (ExerciseTypeId) REFERENCES ExerciseType(Id)
+)
---
+++ src/Sebastian.Database/up/0002_CreateWorkoutTables.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Workout (
+	Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_Workout_Id PRIMARY KEY
+		CONSTRAINT DF_Workout_Id DEFAULT NEWID()
+	, [Name] VARCHAR(128) NOT NULL
+	, DateTimeBegan DATETIME NOT NULL
+	  CONSTRAINT DF_Workout_DateTimeBegan DEFAULT CURRENT_TIMESTAMP 
+	, DateTimeFinished DATETIME
+)
+
+CREATE TABLE WorkoutSuperset (
+	Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_WorkoutSuperset_Id PRIMARY KEY
+		CONSTRAINT DF_WorkoutSuperset_Id DEFAULT NEWID()
+  , WorkoutId UNIQUEIDENTIFIER NOT NULL
+  , Sequence SMALLINT NOT NULL
+  , CONSTRAINT FK_WorkoutSuperset_WorkoutId FOREIGN KEY (WorkoutId) REFERENCES Workout(Id)
+)
+
+CREATE TABLE WorkoutSupersetExercise (
+	Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_WorkoutSupersetExercise_Id PRIMARY KEY
+		CONSTRAINT DF_WorkoutSupersetExercise_Id DEFAULT NEWID()
+	, ExerciseId UNIQUEIDENTIFIER NOT NULL 
+  , WorkoutSupersetId UNIQUEIDENTIFIER NOT NULL
+  , Sequence SMALLINT NOT NULL
+  , CONSTRAINT FK_WorkoutSupersetExercise_ExerciseId FOREIGN KEY (ExerciseId) REFERENCES Exercise(Id)
+  , CONSTRAINT FK_WorkoutSupersetExercise_WorkoutSupersetId FOREIGN KEY (WorkoutSupersetId) REFERENCES WorkoutSuperset(Id)
+)
+
+CREATE TABLE WorkoutSupersetExerciseSet (
+	Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_WorkoutSupersetExerciseSet_Id PRIMARY KEY
+		CONSTRAINT DF_WorkoutSupersetExerciseSet_Id DEFAULT NEWID()
+	, WorkoutSupersetExerciseId UNIQUEIDENTIFIER NOT NULL 
+  , Sequence SMALLINT NOT NULL
+  , CONSTRAINT FK_WorkoutSupersetExerciseSet_WorkoutSupersetExerciseId FOREIGN KEY (WorkoutSupersetExerciseId) REFERENCES WorkoutSupersetExercise(Id)
+)
+
+CREATE TABLE WorkoutSupersetExerciseSetAttribute (
+	Id UNIQUEIDENTIFIER NOT NULL
+		CONSTRAINT PK_WorkoutExerciseSetAttribute_Id PRIMARY KEY
+		CONSTRAINT DF_WorkoutExerciseSetAttribute_Id DEFAULT NEWID()
+  , WorkoutSupersetExerciseSetId UNIQUEIDENTIFIER NOT NULL
+    FOREIGN KEY REFERENCES WorkoutSupersetExerciseSet(Id)
+  , [Name] VARCHAR(128) NOT NULL
+  , Value VARCHAR(128)
+  , CONSTRAINT FK_WorkoutSupersetExerciseSetAttribute_WorkoutSupersetExerciseSetId FOREIGN KEY (WorkoutSupersetExerciseSetId) REFERENCES WorkoutSupersetExerciseSet(Id)
+)
