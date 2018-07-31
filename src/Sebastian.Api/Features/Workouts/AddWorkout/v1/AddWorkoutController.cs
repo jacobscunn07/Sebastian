25ad669a2f89437b01bf7f0b54e18c3bb1f3f99a
Jacob Cunningham <jacob.cunningham@live.com>
2018-07-30 20:52:38 -0500

    Added a controller and versioned api route for creating a new workout

---
+++ src/Sebastian.Api/Features/Workouts/AddWorkout/v1/AddWorkoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sebastian.Api.Features.Workouts.AddWorkout.v1
+{
+    public class AddWorkoutController : Controller
+    {
+        [HttpPost("api/v1/workouts")]
+        public IActionResult Index()
+        {
+            return Json(new {value = "test"});
+        }
+    }
+}
