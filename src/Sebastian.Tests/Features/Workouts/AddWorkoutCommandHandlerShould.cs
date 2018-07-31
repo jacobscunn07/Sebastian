13bfde99ea3f1bdb54624da594430ef388e457fe
Jacob Cunningham <jacob.cunningham@live.com>
2018-07-30 21:11:18 -0500

    Added AddWorkoutCommandHandler Test to tests project

---
+++ src/Sebastian.Tests/Features/Workouts/AddWorkoutCommandHandlerShould.cs
@@ -0,0 +1,20 @@
+﻿namespace Sebastian.Tests.Features.Workouts
+{
+    public class AddWorkoutCommandHandlerShould
+    {
+        public void SaveWorkout()
+        {
+            
+        }
+
+        public void NotSaveWorkoutIfNameIsNull()
+        {
+            
+        }
+        
+        public void NotSaveWorkoutIfNameIsEmpty()
+        {
+            
+        }
+    }
+}
