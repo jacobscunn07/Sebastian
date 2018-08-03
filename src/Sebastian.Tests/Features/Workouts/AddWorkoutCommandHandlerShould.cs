4ba8f24b8885f85d0864e625970495cbcf44a774
Jacob Cunningham <jacob.cunningham@live.com>
2018-08-02 20:48:25 -0500

    Added base testing pattern that uses asp.net core dependency injection

--- src/Sebastian.Tests/Features/Workouts/AddWorkoutCommandHandlerShould.cs
+++ src/Sebastian.Tests/Features/Workouts/AddWorkoutCommandHandlerShould.cs
@@ -1,10 +1,22 @@
-﻿namespace Sebastian.Tests.Features.Workouts
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Sebastian.Api.Features.Workouts.AddWorkout.v1;
+
+namespace Sebastian.Tests.Features.Workouts
 {
     public class AddWorkoutCommandHandlerShould
     {
         public void SaveWorkout()
         {
-            
+            Testing.Action(container =>
+            {
+                var mediator = container.GetService<IMediator>();
+                var command = new AddWorkoutCommand
+                {
+                    Name = "Jacob's Awesome Workout"
+                };
+                mediator.Send(command);
+            });
         }
 
         public void NotSaveWorkoutIfNameIsNull()
---
+++ src/Sebastian.Tests/Testing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sebastian.Api;
+
+namespace Sebastian.Tests
+{
+    public static class Testing
+    {
+        private static readonly IServiceScopeFactory ScopeFactory;
+        public static IConfigurationRoot Configuration { get; }
+        
+        static Testing()
+        {
+            Configuration = new ConfigurationBuilder().Build();
+
+            var app = new Startup(Configuration);
+            var serviceCollection = new ServiceCollection();
+            
+            app.ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            ScopeFactory = serviceProvider.GetService<IServiceScopeFactory>();
+        }
+
+        public static void Action(Action<IServiceProvider> action)
+        {
+            using (var scope = ScopeFactory.CreateScope())
+            {
+                try
+                {
+                    action(scope.ServiceProvider);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
