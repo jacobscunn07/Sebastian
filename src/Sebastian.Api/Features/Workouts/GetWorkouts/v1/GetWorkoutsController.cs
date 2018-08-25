6ad97fb61cf5c20c35acd65c795a5be9b4a37e5b
Jacob Cunningham <jacob.cunningham@live.com>
2018-08-24 23:44:04 -0500

    Added controller endpoints for GET workouts and GET workout

---
+++ src/Sebastian.Api/Features/Workouts/GetWorkouts/v1/GetWorkoutsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sebastian.Api.Features.Workouts.GetWorkouts.v1
+{
+    [Route("api/v1/workouts")]
+    public class GetWorkoutsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public GetWorkoutsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAll()
+        {
+            return Json("Test");
+        }
+
+        [HttpGet("{workoutId}")]
+        public IActionResult Get(Guid workoutId)
+        {
+            return Json("Test");
+        }
+    }
+}
