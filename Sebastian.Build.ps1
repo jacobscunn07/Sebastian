09a6cd1c4a2649662e01182a25370a096673e799
Jacob Cunningham <jacob.cunningham@live.com>
2019-01-24 18:04:15 -0600

    Added ci task to ps commands

--- Sebastian.Build.ps1
+++ Sebastian.Build.ps1
@@ -13,32 +13,41 @@ $databaseProjPath = "$srcPath/Sebastian.Database"
 $devConnectionString = Get-ConnectionString $apiProject;
 $testConnectionString = Get-ConnectionString $testProject;
 
-Task Build -Before Run-Tests {
+# Synopsis: Builds solution and runs integration tests
+Task CI {}
+
+# Synopsis: Builds solution
+Task Build -Before Run-Tests, CI {
     Set-Location -Path $srcPath
     dotnet build --configuration $configuration
     Set-Location -Path $basePath
 }
 
-Task Run-Tests {
+# Synopsis: Runs integration tests
+Task Run-Tests -Before CI {
     Set-Location -Path $testProject
     dotnet fixie
     Set-Location -Path $basePath
 }
 
+# Synopsis: Drops and recreates dev database
 Task Reset-Dev-Database {
     Drop-Database $devConnectionString
     Update-Database $devConnectionString DEV $databaseProjPath
 }
 
+# Synopsis: Runs new migration scripts on dev database
 Task Update-Dev-Database {
     Update-Database $devConnectionString DEV $databaseProjPath
 }
 
-Task Reset-Test-Database {
+# Synopsis: Drops and recreates test database
+Task Reset-Test-Database -Before Run-Tests, CI {
     Drop-Database $testConnectionString
     Update-Database $testConnectionString TEST $databaseProjPath
 }
 
+# Synopsis: Runs new migration scripts on test database
 Task Update-Test-Database {
     Update-Database $testConnectionString TEST $databaseProjPath
 }
